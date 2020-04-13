7cf0b0aaa64970aa95e4a59b3a090cd7d9fd724f
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-13 09:19:18 -0500

    Adding PaginationException and refactor namespace IPaginable

---
+++ Pagination/Exceptions/PaginationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Luilliarcec.Pagination.Exceptions
+{
+    public class PaginationException : Exception
+    {
+        //
+        // Resumen:
+        //     Inicializa una nueva instancia de la clase System.Exception.
+        public PaginationException() : base()
+        {
+        }
+
+        //
+        // Resumen:
+        //     Inicializa una nueva instancia de la clase System.Exception con el mensaje de
+        //     error especificado.
+        //
+        // Parámetros:
+        //   message:
+        //     Mensaje que describe el error.
+        public PaginationException(string message) : base(message)
+        {
+        }
+    }
+}
